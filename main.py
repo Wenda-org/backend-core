3cd10c28c8eee6c35ceaca0c8bc6d06f48e11714
Victor Leonel <vicor32leonel@gmailcom>
2025-10-27 15:33:54 +0100

    cors

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth
 from app.database.connection import Base, engine
 from app.models import user
 
 app = FastAPI(title="Wenda API - Auth Module")
+
+origins = [
+    "http://127.0.0.1:8000",
+     "https://backend-core-rzxx.onrender.com", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # Permite apenas essas origens
+    allow_credentials=True,           # Permite envio de cookies/autenticação
+    allow_methods=["*"],              # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],              # Permite todos os cabeçalhos
+)
 
 Base.metadata.create_all(bind=engine)
 
